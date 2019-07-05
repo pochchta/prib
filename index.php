fa3cc37f820eb0e27bb3074772863c69d494dad3
pochchta <pochchta@rambler.ru>
2019-07-05 16:46:38 +0800

    разделение кода и данных в index.php

--- index.php
+++ index.php
@@ -1,44 +1,12 @@
 <?php 
+include 'model.php';
 
-require 'db.php';
-//include 'libs/phpqrcode.php';
-include 'libs/phpqrcode/qrlib.php';
-
+include 'tpl/head.html';
+if ( isset( $_SESSION['logged_user'] ) ) {
+    include 'tpl/header/user.html';
+} else {
+    include 'tpl/header/guest.html';
+}
+include 'tpl/body/one.html';
+include 'tpl/footer.html'
 ?>
-
-<!DOCTYPE html>
-<html lang="RU">
-<head>
-    <meta charset="UTF-8">
-    <title>Главная</title>
-    <link href="css/style.css" type="text/css" rel="stylesheet"/>
-</head>
-<body>
-
-    <div class="header">
-        <div class="name">
-            Главная страница
-        </div>
-        <div class="links">
-
-            <?php 
-            if ( isset( $_SESSION['logged_user'] ) ) {
-                echo '<a href="profile.php">'.$_SESSION['logged_user']->login.'</a>';
-                echo '<a href="logout.php">Выйти</a>';
-            } else {
-                echo '<a href="login.php">Войти</a>';
-                echo '<a href="signup.php">Зарегистрироваться</a>';
-            }
-            ?>
-
-        </div>
-    </div>
-    <hr>    
-
-<?php 
-    QRcode::png( 'http://localhost/index.php?n=1' , 'img/1.png' , 'H');
- ?>
- <img src="img/1.png">
-
-</body>
-</html>
---
+++ model.php
@@ -0,0 +1,7 @@
+<?php 
+require 'db.php';
+//include 'libs/phpqrcode.php';
+include 'libs/phpqrcode/qrlib.php';
+
+//QRcode::png( 'http://localhost/index.php?n=1' , 'img/1.png' , 'H');
+ ?>
