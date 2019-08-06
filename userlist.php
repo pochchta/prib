f4c149ae82e138425474e0199fadcb7e423f9da1
pochchta <pochchta@rambler.ru>
2019-08-06 16:45:09 +0800

    исправлена ошибка - не работали сообщения

--- userlist.php
+++ userlist.php
@@ -1,13 +1,11 @@
 <?php
 	$title = 'Список пользователей';
 	include 'model.php';
-	$perm_userlist = false;
-	if ( test_perm('r_users') ){
-		$perm_userlist = true;
-		if ( empty($_SESSION['users']->limit) ) {
+	if ( test_perm('r_users') )
+		if ( empty($_SESSION['users']->limit) )
 			$_SESSION['users'] = new table_settings;
-		}
-		if ( ! empty($_GET) ) {		// изменение страницы отображения списка пользователей
+	if ( ! empty($_GET) ) {									// изменение страницы отображения списка пользователей
+		if ( test_perm('r_users') ){
 			if ( isset($_GET['del']) )   		            									// удаление юзера
 				if ( test_perm('w_user_del') )
 					del_fields( $_GET['del'] , 'users' );
@@ -16,7 +14,9 @@
 					state_fields( $_GET['st'] , 'users' ); 		           
 			if ( isset($_GET['lu']) ) limit_fields( $_GET['lu'] , 'users' ); 					// лимит записей на страницу
 			header('Location: /userlist.php');	// для очистки адресной строки
-		} else {
+		}
+	} else {
+		if ( test_perm('r_users') ){
 			if ( isset($_POST['n_page']) )   	  page_fields( $_POST['n_page'] , 'users' ); 		// номер страницы
 			if ( isset($_POST['do_sort']) ) 	  sort_fields( $_POST , 'users' );					// сортировка
 			if ( isset($_POST['do_find']) ) 	  find_fields( $_POST , 'users' );				    // поиск
@@ -25,16 +25,16 @@
 			else $find_form = $_SESSION['users']->find_form;
 			$info = list_fields( 'users' );
 		}
-	}	
-	include 'tpl/head.html';
-	include 'tpl/errors.html';
-	include 'tpl/message.html';
-	if ( isset($_SESSION['logged_user']) ) {
-		include 'tpl/header/user.html';
-	} else {
-		include 'tpl/header/guest.html';
+		include 'tpl/head.html';
+		include 'tpl/errors.html';
+		include 'tpl/message.html';
+		if ( isset($_SESSION['logged_user']) ) {
+			include 'tpl/header/user.html';
+		} else {
+			include 'tpl/header/guest.html';
+		}
+		if ( test_perm('r_users') )
+			include 'tpl/body/userlist.html';
+		include 'tpl/footer.html';
 	}
-	if ( $perm_userlist )
-		include 'tpl/body/userlist.html';
-	include 'tpl/footer.html';
 ?>
